93ce5797c422d50498c29f1a49473605e596eb7f
Abner André <abnerndr.dev@gmail.com>
2023-06-15 11:53:21 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    vercel: '',
+    baseURL: '',
+    siteMeta: {
+      url: '',
+      domain: '',
+      title: '',
+      description: '',
+      imageUrl: ''
+    }
+  },
+  images: {
+    // dominios da aplicação
+    domains: ''
+  },
+  eslint: {
+    dirs: ['src']
+  },
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
 }
 
 module.exports = nextConfig
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({ weight: ['400', '500', '600', '700'], style: 'normal' });
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
+      <body className={`${poppins.className}`}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
