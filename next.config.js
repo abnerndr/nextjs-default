8997cbaadbf013bd4008beee3f5033a632c98ce1
Abner André <abnerndr.dev@gmail.com>
2023-06-15 13:56:11 -0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    vercel: '',
-    baseURL: '',
-    siteMeta: {
-      url: '',
-      domain: '',
-      title: '',
-      description: '',
-      imageUrl: ''
-    }
-  },
-  images: {
-    // dominios da aplicação
-    domains: ''
-  },
+  // env: {
+  //   vercel: '',
+  //   baseURL: '',
+  //   siteMeta: {
+  //     url: '',
+  //     domain: '',
+  //     title: '',
+  //     description: '',
+  //     imageUrl: ''
+  //   }
+  // },
+  // images: {
+  //   // dominios da aplicação
+  //   domains: ''
+  // },
   eslint: {
     dirs: ['src']
   },
